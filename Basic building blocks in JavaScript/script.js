d44d52b0b8fae8104ee18a1e5c1bbc3212883d2a
denyscz <127204299+denyscz@users.noreply.github.com>
2023-03-28 22:33:39 +0200

    j

---
+++ Basic building blocks in JavaScript/script.js
@@ -0,0 +1,38 @@
+let bookCount = 10;
+for (let i = 0; i < bookCount; i++) {
+
+}
+let numberCount = 10
+for(let i=1; i < numberCount; i++) {
+
+};
+let payBill = (amount, beneficiary) => {
+
+}
+let add = (a, b) => {
+    return a + b;
+}
+console.log(add(1,2))
+let printNumbersTill = 20
+console.log(printNumbersTill)
+for ( let i = 1; i <= 20; i++) {
+    console.log(i)
+}
+
+let letter = "N";
+console.log(letter)
+let text = "printNumbersTill";
+let position = text.search(/N/)
+console.log(position);
+
+let names = ["Mark"];
+names.forEach((name) => {
+    console.log(`Hello, ${name}!`);
+});
+
+let printValues = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+    }
+  };
+
